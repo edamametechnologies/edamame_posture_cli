da820be587ba552ffc447a661325dc7199d88497
Frank Lyonnet <flyonnet@edamame.tech>
2024-07-14 13:06:03 +0200

    FIX: CI/CD

--- windows/sign.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-APP_PATH="$1"
-
-AzureSignTool sign -kvu "${AZURE_SIGN_KEY_VAULT_URI}" -kvi "${AZURE_SIGN_CLIENT_ID}" -kvt "${AZURE_SIGN_TENANT_ID}" -kvs "${AZURE_SIGN_CLIENT_SECRET}" -kvc ${AZURE_SIGN_CERT_NAME} -tr http://timestamp.digicert.com -v "$APP_PATH"
