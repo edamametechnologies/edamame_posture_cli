309a8269bf6ce3989cb9c618ff6e52ec95826d34
Frank Lyonnet <flyonnet@edamame.tech>
2024-07-23 18:32:07 +0200

    ADD: use GitHub run ID as the device ID

--- src/main.rs
+++ src/main.rs
@@ -253,7 +253,7 @@ fn run() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() > 1 && args[1] == "background-process" {
         // Debug logging
-        std::env::set_var("EDAMAME_LOG_LEVEL", "debug");
+        //std::env::set_var("EDAMAME_LOG_LEVEL", "debug");
 
         // Reporting and community are on
         initialize(
