ac0b9cc31c4a5beb38a05e4c80ef431ec66caaa5
Frank Lyonnet <flyonnet@edamame.tech>
2024-07-23 14:47:41 +0200

    REFACTOR: changed timeout

--- src/main.rs
+++ src/main.rs
@@ -341,7 +341,7 @@ fn run_base() {
         Some(("wait-for-success", sub_matches)) => {
             let timeout = sub_matches
                 .get_one::<u64>("TIMEOUT")
-                .unwrap_or_else(|| &120);
+                .unwrap_or_else(|| &180);
             handle_wait_for_success(*timeout)
         }
         Some(("get-core-info", _)) => handle_get_core_info(),
