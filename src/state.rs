c07fda4afd862d1b8bbdb3a6f7df2fb8c120e30d
Frank Lyonnet <flyonnet@edamame.tech>
2024-09-02 09:34:56 +0200

    FIX: score and lanscan in the posture state instead of the core own state (that is cached)

--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,5 @@
+use edamame_core::api::api_lanscan::LANScanAPI;
+use edamame_core::api::api_score::ScoreAPI;
 use fs2::FileExt;
 use serde::{Deserialize, Serialize};
 use std::fs;
@@ -9,11 +11,28 @@ use std::path::PathBuf;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct State {
     pub pid: Option<u32>,
-    pub handle: Option<u64>, // Add handle for Windows
+    pub handle: Option<u64>,
     pub is_success: bool,
     pub connected_domain: String,
     pub connected_user: String,
     pub last_network_activity: String,
+    pub score: ScoreAPI,
+    pub devices: LANScanAPI,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State {
+            pid: None,
+            handle: None,
+            is_success: false,
+            connected_domain: "".to_string(),
+            connected_user: "".to_string(),
+            last_network_activity: "".to_string(),
+            score: ScoreAPI::default(),
+            devices: LANScanAPI::default(),
+        }
+    }
 }
 
 impl State {
@@ -25,7 +44,14 @@ impl State {
             let mut contents = String::new();
             file.read_to_string(&mut contents)
                 .expect("Unable to read state file");
-            let state: State = serde_yaml::from_str(&contents).expect("Unable to parse state file");
+            let state: State = match serde_yaml::from_str(&contents) {
+                Ok(state) => state,
+                Err(e) => {
+                    eprintln!("Unable to deserialize state: {}, cleaning it", e);
+                    Self::clear();
+                    State::default()
+                }
+            };
             file.unlock().expect("Unable to unlock file");
             state
         } else {
@@ -36,6 +62,8 @@ impl State {
                 connected_domain: "".to_string(),
                 connected_user: "".to_string(),
                 last_network_activity: "".to_string(),
+                score: ScoreAPI::default(),
+                devices: LANScanAPI::default(),
             }
         }
     }
@@ -44,7 +72,7 @@ impl State {
         let path = Self::state_file_path();
         let mut file = File::create(&path).expect("Unable to create state file");
         file.lock_exclusive()
-            .expect("Unable to lock file for writing");
+            .expect("Unable to lock file for writing, is another instance running?");
         let contents = serde_yaml::to_string(self).expect("Unable to serialize state");
         file.write_all(contents.as_bytes())
             .expect("Unable to write state file");
