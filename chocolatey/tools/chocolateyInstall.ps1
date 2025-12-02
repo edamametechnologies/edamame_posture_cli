a04e89b0bb8ab282162c5e8ba067883f811fb90f
EDAMAME Dev Local <dev@edamame.tech>
2025-12-02 20:19:12 +0100

    Refactor Chocolatey installation and uninstallation scripts for EDAMAME Posture package This commit updates the Chocolatey installation script to download the standalone executable and create a shim for easier access. Additionally, it modifies the uninstallation script to remove the binary file if it exists, ensuring a cleaner uninstall process. These changes enhance the package management experience and improve the reliability of the installation and uninstallation workflows.

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -4,11 +4,17 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64 = "https://github.com/edamametechnologies/edamame_posture_cli/releases/download/v$packageVersion/edamame_posture-$packageVersion-x86_64-pc-windows-msvc.exe"
 $checksum64 = 'a462edd862ff1d45378a41480d9219a0bcb867a05b5f735cfa3ed60c13051582'
 
-Install-ChocolateyPackage -PackageName $packageName `
-                          -FileType 'exe' `
-                          -Url64bit $url64 `
-                          -Checksum64 $checksum64 `
-                          -ChecksumType64 'sha256'
+# Download the standalone executable
+$fileFullPath = Join-Path $toolsDir "edamame_posture.exe"
+Get-ChocolateyWebFile -PackageName $packageName `
+                      -FileFullPath $fileFullPath `
+                      -Url64bit $url64 `
+                      -Checksum64 $checksum64 `
+                      -ChecksumType64 'sha256'
+
+# Install-ChocolateyBinaryFile creates a shim so the executable is available in PATH
+Install-ChocolateyBinaryFile -PackageName $packageName `
+                             -FileFullPath $fileFullPath
 
 
 
