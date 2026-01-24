bea23aceb245fe0166c19975e2cfd0855550ff51
EDAMAME Dev Local <dev@edamame.tech>
2026-01-25 00:11:53 +0100

    Pre-publish

--- homebrew/edamame-posture.rb
+++ homebrew/edamame-posture.rb
@@ -1,9 +1,9 @@
 class EdamamePosture < Formula
   desc "EDAMAME Security posture analysis and remediation"
   homepage "https://edamame.tech"
-  url "https://github.com/edamametechnologies/edamame_posture_cli/releases/download/v0.9.81/edamame_posture-0.9.81-universal-apple-darwin"
+  url "https://github.com/edamametechnologies/edamame_posture_cli/releases/download/v1.0.0/edamame_posture-1.0.0-universal-apple-darwin"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
-  version "0.9.81"
+  version "1.0.0"
   license "Apache-2.0"
 
   def install
