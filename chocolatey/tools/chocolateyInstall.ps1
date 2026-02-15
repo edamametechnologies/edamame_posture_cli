cde006ad8111511ee4729ebe5661ef00786e9c73
EDAMAME Dev Local <dev@edamame.tech>
2026-02-15 23:02:34 +0100

    Pre-publish

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'edamame-posture'
-$packageVersion = '1.0.4'
+$packageVersion = '1.0.5'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64 = "https://github.com/edamametechnologies/edamame_posture_cli/releases/download/v$packageVersion/edamame_posture-$packageVersion-x86_64-pc-windows-msvc.exe"
 $checksum64 = 'a462edd862ff1d45378a41480d9219a0bcb867a05b5f735cfa3ed60c13051582'
