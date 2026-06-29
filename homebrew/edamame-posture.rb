ea47f600ccc82e68d5e60e6e22f103f51b881af9
EDAMAME Dev Local <dev@edamame.tech>
2026-06-29 10:31:49 +0200

    Pre-publish

--- homebrew/edamame-posture.rb
+++ homebrew/edamame-posture.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 cask "edamame-posture" do
-  version "1.4.7"
+  version "1.5.0"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
 
   url "https://github.com/edamametechnologies/edamame_posture_cli/releases/download/v#{version}/edamame-posture-macos-#{version}.pkg"
